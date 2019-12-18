0235b978c64c11eab9c6e313c30a669a495f9e50
Piotr Rydwański <pioryd123@gmail.com>
2019-12-18 21:49:03 +0100

    Add config class

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const { Util } = require("./src/util");
 const { ModulesManager } = require("./src/modules_manager");
 const { setup_exit_handlers } = require("./src/application");
 const { Server } = require("./src/net/server");
+const { Client } = require("./src/net/client");
+const { Config } = require("./src/config");
 
-module.exports = { Util, ModulesManager, setup_exit_handlers, Server };
+module.exports = {
+  Util,
+  ModulesManager,
+  setup_exit_handlers,
+  Server,
+  Client,
+  Config
+};
---
+++ src/config.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const { Util } = require("./util");
+
+// Config i async
+class Config {
+  constructor({ file_full_name, on_update }) {
+    this.file_full_name = file_full_name;
+    this.on_update = on_update;
+    this.data = null;
+    this.watcher = null;
+  }
+
+  _read_data_from_file() {
+    this.data = Util.read_from_json(this.file_full_name);
+  }
+
+  load() {
+    this._read_data_from_file();
+
+    this.watcher = fs.watch(this.file_full_name, (curr, prev) => {
+      this.on_update();
+      this._read_data_from_file();
+    });
+  }
+
+  save() {
+    if (this.data != null) Util.write_to_json(this.file_full_name, this.data);
+  }
+
+  terminate() {
+    if (this.watcher != null) this.watcher.close();
+    this.save();
+  }
+}
+
+module.exports = { Config };
