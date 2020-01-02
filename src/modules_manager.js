56c79583d1465bb60e44495292210ca277e6b118
Piotr Rydwański <pioryd123@gmail.com>
2020-01-02 23:39:31 +0100

    Rebuild module manager

--- src/modules_manager.js
+++ src/modules_manager.js
@@ -11,17 +11,21 @@ const EVENTS_LIST = [
 
 class ModulesManager {
   constructor({
-    application,
-    event_emiter,
-    modules_directory,
+    config,
+    event_emitter,
+    root_path,
+    modules_map,
+    paths_auto_find,
     disabled_modules
   }) {
-    this.application = application;
-    this.event_emiter = event_emiter;
-    this.modules_directory = modules_directory;
+    this.config = config;
+    this.event_emitter = event_emitter;
+    this.root_path = root_path;
+    this.modules_map = modules_map;
+    this.paths_auto_find = paths_auto_find;
+    this.disabled_modules = disabled_modules;
     this.modules_list = {};
     this.event_module_bounds = {};
-    this.disabled_modules = disabled_modules;
   }
 
   add_event(event_name, module_name) {
@@ -33,7 +37,7 @@ class ModulesManager {
     this.event_module_bounds[module_name][event_name] = (...args) => {
       this.modules_list[module_name][event_name](...args);
     };
-    this.event_emiter.on(
+    this.event_emitter.on(
       event_name,
       this.event_module_bounds[module_name][event_name]
     );
@@ -43,7 +47,7 @@ class ModulesManager {
     if (!(event_name in this.modules_list[module_name])) return;
     if (!(event_name in this.event_module_bounds[module_name])) return;
 
-    this.event_emiter.off(
+    this.event_emitter.off(
       event_name,
       this.event_module_bounds[module_name][event_name]
     );
@@ -64,24 +68,37 @@ class ModulesManager {
   }
 
   load_modules() {
-    let modules_names = Util.get_directories(this.modules_directory);
+    for (const modules_path of this.paths_auto_find) {
+      let modules_names = Util.get_directories(modules_path);
 
-    for (const module_name of modules_names) {
-      if (this.disabled_modules.includes(module_name)) continue;
-
-      const module_path = path.join(this.modules_directory, module_name);
-
-      if (Util.is_path_exist(module_path)) {
-        let fw_module = require(module_path);
-        let fw_module_class_name = Object.entries(fw_module)
-          .values()
-          .next().value[0];
-        this.modules_list[module_name] = new fw_module[fw_module_class_name]({
-          application: this.application
-        });
-        this.init_module(module_name);
-      } else log.info("Module path does NOT exist: " + module_path);
+      for (const module_name of modules_names)
+        this.load_module(module_name, modules_path);
     }
+
+    for (const [module_name, module_path] of Object.entries(this.modules_map))
+      this.load_module(module_name, module_path);
+  }
+
+  load_module(module_name, module_path) {
+    if (this.disabled_modules.includes(module_name)) return;
+
+    const module_full_path = path.join(
+      this.root_path,
+      module_path,
+      module_name
+    );
+
+    if (Util.is_path_exist(module_full_path)) {
+      let fw_module = require(module_full_path);
+      let fw_module_class_name = Object.entries(fw_module)
+        .values()
+        .next().value[0];
+      this.modules_list[module_name] = new fw_module[fw_module_class_name]({
+        event_emitter: this.event_emitter,
+        config: this.config
+      });
+      this.init_module(module_name);
+    } else log.info("Module path does NOT exist: " + module_path);
   }
 }
 
