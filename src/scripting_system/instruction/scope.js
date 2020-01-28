ef094040bb57be1e8055c5d64e1b0cc692418959
Piotr Rydwański <pioryd123@gmail.com>
2020-01-28 19:02:24 +0100

    Create scope.js

---
+++ src/scripting_system/instruction/scope.js
@@ -0,0 +1,72 @@
+const { RETURN_CODE } = require("./return_code");
+
+class Scope {
+  constructor() {
+    this._childs = [];
+    this._current_child_index = 0;
+    this._debug_info = {};
+  }
+
+  _reset() {
+    this._current_child_index = 0;
+  }
+
+  process(script, root) {
+    // Internal:goto
+    if (script._goto_find.enabled) {
+      this._current_child_index = 0;
+      while (this._current_child_index < this._childs.length) {
+        const { return_code } = this._childs[this._current_child_index].process(
+          script,
+          root
+        );
+        if (return_code === RETURN_CODE.PROCESSED) this._current_child_index++;
+
+        if (!script._goto_find.enabled) {
+          if (this._current_child_index < this._childs.length) {
+            return { return_code: RETURN_CODE.PROCESSING };
+          } else {
+            this._reset();
+            return { return_code: RETURN_CODE.PROCESSED };
+          }
+        }
+      }
+
+      this._reset();
+      return { return_code: RETURN_CODE.PROCESSED };
+    }
+
+    if (this._current_child_index < this._childs.length) {
+      const { return_code, internal } = this._childs[
+        this._current_child_index
+      ].process(script, root);
+
+      if (
+        internal === "goto" ||
+        internal === "break" ||
+        internal === "continue"
+      ) {
+        this._reset();
+        return { return_code: RETURN_CODE.PROCESSING, internal };
+      } else if (return_code === RETURN_CODE.PROCESSED) {
+        this._current_child_index++;
+      } else if (return_code === RETURN_CODE.PROCESSING) {
+        return { return_code };
+      }
+    }
+
+    if (this._current_child_index < this._childs.length) {
+      return { return_code: RETURN_CODE.PROCESSING };
+    } else {
+      this._reset();
+      return { return_code: RETURN_CODE.PROCESSED };
+    }
+  }
+
+  _debug(arg) {
+    console.log();
+    return arg;
+  }
+}
+
+module.exports = Scope;
