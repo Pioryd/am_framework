a24d7d5d810ad908a71cbb8c1cb7fdae7103d6ef
Piotr Rydwański <pioryd123@gmail.com>
2020-01-28 19:02:46 +0100

    Create internal.js

---
+++ src/scripting_system/instruction/internal.js
@@ -0,0 +1,47 @@
+const { RETURN_CODE } = require("./return_code");
+
+/**
+ * Commands:
+ *    - must be lower case.
+ *    - works only in current script
+ *
+ * List:
+ *    - [break] and [continue]
+ *      - Works for [Scope_WHILE] and [Scope_FOR].
+ *    - [label label_name ] and [goto label_name]
+ *      - [All scopes]
+ *        - set child_index at found label
+ *      - [Scope_FOR]
+ *        - Do NOT init loop.
+ *    - [sleep time_in_milliseconds]
+ *      - Start counting on called in script
+ */
+class Internal {
+  constructor() {
+    this._command = "";
+    this._arg = "";
+  }
+
+  process(script, root) {
+    if (script._goto_find.enabled) {
+      if (
+        this._command === "label" &&
+        this._arg === script._goto_find.label_name
+      ) {
+        script._goto_find.enabled = false;
+      }
+
+      return { return_code: RETURN_CODE.PROCESSED };
+    }
+
+    if (this._command === "sleep") {
+      script.sleep(this._arg);
+    } else if (this._command === "goto") {
+      script.goto(this._arg);
+    }
+
+    return { return_code: RETURN_CODE.PROCESSED, internal: this._command };
+  }
+}
+
+module.exports = Internal;
