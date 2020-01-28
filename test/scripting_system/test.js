a07ad602b94219bc0dcac4d7d52ff2c32a4138e0
Piotr Rydwański <pioryd123@gmail.com>
2020-01-28 19:03:48 +0100

    Add script system tests

---
+++ test/scripting_system/test.js
@@ -0,0 +1,388 @@
+const { expect, config } = require("chai");
+const { Util } = require("../../src/util");
+const { Stopper } = require("../../src/stopper");
+const path = require("path");
+const Script = require("../../src/scripting_system/instruction/script");
+const {
+  RETURN_CODE
+} = require("../../src/scripting_system/instruction/return_code");
+
+const script_full_name = path.join(__dirname, "script.json");
+
+let source = {};
+let root = {};
+describe("Scripting system test", () => {
+  before(() => {
+    source = Util.read_from_json(script_full_name);
+  });
+  // it("Test instruction - JS", () => {
+  //   const script_name = "Test_js";
+
+  //   const script = new Script({
+  //     name: script_name,
+  //     source: source[script_name],
+  //     root
+  //   });
+
+  //   expect(script.data.val).to.equal(0);
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //   }
+  //   expect(script.data.val).to.equal(1);
+  // });
+  // it("Test instruction - Scope", () => {
+  //   const script_name = "Test_scope";
+  //   const script = new Script({
+  //     name: script_name,
+  //     source: source[script_name],
+  //     root
+  //   });
+
+  //   expect(script.data.val).to.equal(0);
+  //   expect(script._root_scope._current_child_index).to.equal(0);
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   expect(script._root_scope._current_child_index).to.equal(2);
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //   }
+  //   expect(script._root_scope._current_child_index).to.equal(0);
+  //   expect(script.data.val).to.equal(3);
+  // });
+  // describe("Test instruction - Scope_IF", () => {
+  //   it("First condition", () => {
+  //     const script_name = "Test_scope_if";
+  //     const script = new Script({
+  //       name: script_name,
+  //       source: source[script_name],
+  //       root
+  //     });
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //     }
+  //     expect(script.data.val).to.deep.equal(3);
+  //   });
+  //   it("Second condition", () => {
+  //     const script_name = "Test_scope_if";
+  //     const script = new Script({
+  //       name: script_name,
+  //       source: source[script_name],
+  //       root
+  //     });
+
+  //     script.data.val = 1;
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //     }
+  //     expect(script.data.val).to.deep.equal(5);
+  //   });
+  //   it("Third condition", () => {
+  //     const script_name = "Test_scope_if";
+  //     const script = new Script({
+  //       name: script_name,
+  //       source: source[script_name],
+  //       root
+  //     });
+
+  //     script.data.val = 10;
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //     }
+  //     {
+  //       const { return_code } = script.process(null);
+  //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //     }
+  //     expect(script.data.val).to.deep.equal(15);
+  //   });
+  // });
+  // it("Test instruction - Scope_WHILE", () => {
+  //   const script_name = "Test_scope_while";
+  //   const script = new Script({
+  //     name: script_name,
+  //     source: source[script_name],
+  //     root
+  //   });
+
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //   }
+  //   expect(script.data.val).to.deep.equal(4);
+  // });
+  // it("Test instruction - Scope_FOR", () => {
+  //   const script_name = "Test_scope_for";
+  //   const script = new Script({
+  //     name: script_name,
+  //     source: source[script_name],
+  //     root
+  //   });
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   expect(script.data.i).to.deep.equal(1);
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+  //   }
+  //   expect(script.data.i).to.deep.equal(2);
+  //   {
+  //     const { return_code } = script.process(null);
+  //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+  //   }
+  //   expect(script.data.val).to.deep.equal(4);
+  // });
+  describe("Test instruction - Internal", () => {
+    //   it("break", () => {
+    //     const script_name = "Test_internal_break";
+    //     const script = new Script({
+    //       name: script_name,
+    //       source: source[script_name],
+    //       root
+    //     });
+    //     for (let i = 0; i < 4; i++) {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //     }
+    //     expect(script.data.val).to.deep.equal(2);
+    //     expect(script.data.i).to.deep.equal(0);
+    //     for (let i = 0; i < 4; i++) {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //     }
+    //     expect(script.data.val).to.deep.equal(4);
+    //     expect(script.data.val_2).to.deep.equal(1);
+    //     {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+    //     }
+    //     expect(script.data.val).to.deep.equal(4);
+    //     expect(script.data.val_2).to.deep.equal(1);
+    //   });
+    //   it("continue", () => {
+    //     const script_name = "Test_internal_continue";
+    //     const script = new Script({
+    //       name: script_name,
+    //       source: source[script_name],
+    //       root
+    //     });
+    //     Scope for
+    //     for (let i = 0; i < 6; i++) {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //     }
+    //     expect(script.data.i).to.deep.equal(2);
+    //     expect(script.data.val).to.deep.equal(0);
+    //     {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //     }
+    //     expect(script.data.i).to.deep.equal(2);
+    //     expect(script.data.val).to.deep.equal(0);
+    //     Scope while
+    //     for (let i = 0; i < 6; i++) {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //     }
+    //     expect(script.data.val).to.deep.equal(2);
+    //     expect(script.data.val_2).to.deep.equal(0);
+    //     {
+    //       const { return_code } = script.process(null);
+    //       expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+    //     }
+    //     expect(script.data.val).to.deep.equal(2);
+    //     expect(script.data.val_2).to.deep.equal(0);
+    //   });
+    // it("sleep 1000", () => {
+    //   const script_name = "Test_internal_sleep";
+    //   const script = new Script({
+    //     name: script_name,
+    //     source: source[script_name],
+    //     root
+    //   });
+    //   const stopper = new Stopper();
+    //   {
+    //     const { return_code } = script.process(null);
+    //     expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+    //   }
+    //   let while_return_code = RETURN_CODE.PROCESSING;
+    //   stopper.start();
+    //   while (while_return_code === RETURN_CODE.PROCESSING) {
+    //     const { return_code } = script.process(null);
+    //     while_return_code = return_code;
+    //   }
+    //   stopper.stop();
+    //   expect(stopper.get_elapsed_milliseconds()).to.be.at.least(1000);
+    //   expect(script.data.val).to.deep.equal(2);
+    // });
+    it("label and goto", () => {
+      const script_name = "Test_label_and_goto";
+      const script = new Script({
+        name: script_name,
+        source: source[script_name],
+        root
+      });
+      /**
+       *   TODO
+       *   label/goto have so many cases.
+       *   Currently don't have time to make tests for all cases.
+       */
+      // Forward
+      for (let i = 0; i < 7; i++) {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(
+          RETURN_CODE.PROCESSING,
+          `For_Index[${i}]`
+        );
+      }
+      expect(script.data.val).to.deep.equal(0);
+      expect(script.data.val_2).to.deep.equal(0);
+      // Backward
+      for (let i = 0; i < 3; i++) {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(
+          RETURN_CODE.PROCESSING,
+          `For_Index[${i}]`
+        );
+      }
+      expect(script.data.val).to.deep.equal(0);
+      expect(script.data.val_2).to.deep.equal(1);
+      // Forward
+      // for (let i = 0; i < 2; i++) {
+      //   const { return_code } = script.process(null);
+      //   expect(return_code).to.deep.equal(
+      //     RETURN_CODE.PROCESSING,
+      //     `For_Index[${i}]`
+      //   );
+      // }
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+      }
+      //expect(script.data.val).to.deep.equal(2);
+      //expect(script.data.val_2).to.deep.equal(1);
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+      }
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+      }
+      expect(script.data.val).to.deep.equal(2);
+      expect(script.data.val_2).to.deep.equal(1);
+    });
+    it("label and goto", () => {
+      const script_name = "Test_label_and_goto";
+      const script = new Script({
+        name: script_name,
+        source: source[script_name],
+        root
+      });
+      /**
+       *   TODO
+       *   label/goto have so many cases.
+       *   Currently don't have time to make tests for all cases.
+       */
+      // Forward
+      for (let i = 0; i < 7; i++) {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(
+          RETURN_CODE.PROCESSING,
+          `For_Index[${i}]`
+        );
+      }
+      expect(script.data.val).to.deep.equal(0);
+      expect(script.data.val_2).to.deep.equal(0);
+      // Backward
+      for (let i = 0; i < 3; i++) {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(
+          RETURN_CODE.PROCESSING,
+          `For_Index[${i}]`
+        );
+      }
+      expect(script.data.val).to.deep.equal(0);
+      expect(script.data.val_2).to.deep.equal(1);
+      // Forward
+      // for (let i = 0; i < 2; i++) {
+      //   const { return_code } = script.process(null);
+      //   expect(return_code).to.deep.equal(
+      //     RETURN_CODE.PROCESSING,
+      //     `For_Index[${i}]`
+      //   );
+      // }
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+      }
+      //expect(script.data.val).to.deep.equal(2);
+      //expect(script.data.val_2).to.deep.equal(1);
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSING);
+      }
+      {
+        const { return_code } = script.process(null);
+        expect(return_code).to.deep.equal(RETURN_CODE.PROCESSED);
+      }
+      expect(script.data.val).to.deep.equal(2);
+      expect(script.data.val_2).to.deep.equal(1);
+    });
+  });
+});
