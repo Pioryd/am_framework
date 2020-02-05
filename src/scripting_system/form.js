aebbffc3fb612292d5c06b705aa01ea8d2f779a2
Piotr Rydwański <pioryd123@gmail.com>
2020-02-05 23:39:55 +0100

    Some progress in forms - not working yet

--- src/scripting_system/form.js
+++ src/scripting_system/form.js
@@ -1,13 +1,44 @@
 const { RETURN_CODE } = require("./instruction/return_code");
+const parse = require("./instruction/parse");
 
 class Form {
-  constructor() {
+  constructor(source) {
     this.id = null;
+    this._source = source;
+    this._name = "";
+    this._rules = [];
+    this._signals = [];
+    this._events = [];
+    this._running_scripts = {};
+
+    this._parse();
   }
 
   process(root) {
-    script.print_debug(`process Type[Form] ID[${this.id}]`);
     return { return_code: RETURN_CODE.PROCESSED };
+  }
+
+  _run_script(name) {
+    if (name in this._running_scripts) return;
+
+    const script = Object.entries(this._source.scripts)[name];
+    if (script == null) throw "Unable to run script: " + name;
+
+    this._running_scripts[name] = parse(script);
+
+    // on_run_script
+  }
+
+  _parse() {
+    const source = this._source;
+    if (source.name == null || source.id == null)
+      throw "Unable to parse form: " + source;
+
+    this.id = source.id;
+    this._name = source.name;
+    this._rules = source.rules;
+    this._signals = source.signals;
+    this._events = source.events;
   }
 }
 
--- test/scripting_system/forms_test.js
+++ test/scripting_system/forms_test.js
@@ -6,15 +6,31 @@ const {
   RETURN_CODE
 } = require("../../src/scripting_system/instruction/return_code");
 
-const script_full_name = path.join(__dirname, "forms_test.json");
+const forms_full_name = path.join(__dirname, "forms_test.json");
 
 let root = {
-  forms: {}
+  forms_source: {}
 };
 
 describe("Forms test", () => {
   before(() => {
-    root.forms = Util.read_from_json(script_full_name);
+    const forms_source_array = Util.read_from_json(forms_full_name);
+    for (const source of forms_source_array)
+      root.forms_source[source.name] = source;
   });
-  it("First test", () => {});
+  it("Parse form", () => {
+    const form = new Form(root.forms_source["Test_1"]);
+
+    expect(form.id).to.equal("Test_1_ID");
+    expect(form._name).to.equal("Test_1");
+    expect(form._rules.length).to.equal(1);
+    expect(form._signals.length).to.equal(1);
+    expect(form._events.length).to.equal(1);
+    expect(Object.keys(form._running_scripts).length).to.equal(0);
+  });
+
+  it("Process form", () => {
+    const form = new Form(root.forms_source["Test_1"]);
+    form.process(root);
+  });
 });
