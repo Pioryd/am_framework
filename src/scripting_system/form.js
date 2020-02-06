21c9428dd39c5f8959ae6dfe7431ff3cdf9b3eb3
Piotr Rydwański <pioryd123@gmail.com>
2020-02-06 14:59:40 +0100

    Work on forms - not working yet

--- src/scripting_system/form.js
+++ src/scripting_system/form.js
@@ -1,8 +1,9 @@
 const { RETURN_CODE } = require("./instruction/return_code");
 const parse = require("./instruction/parse");
+const EventEmitter = require("events");
 
 class Form {
-  constructor(source) {
+  constructor(root, source) {
     this.id = null;
     this._source = source;
     this._name = "";
@@ -11,10 +12,23 @@ class Form {
     this._events = [];
     this._running_scripts = {};
 
+    this.root = root;
+    this.event_emitter = new EventEmitter();
+
     this._parse();
+    this.event_emitter.emit("form_init");
   }
 
   process(root) {
+    // Proces running scripts
+    for (const [name, script] of Object.entries(this._running_scripts)) {
+      const return_value = script.process(null, root);
+      if (return_value.return_code === RETURN_CODE.PROCESSED) {
+        delete this._running_scripts[name];
+        this.event_emitter.emit("script_processed", name);
+      }
+    }
+
     return { return_code: RETURN_CODE.PROCESSED };
   }
 
@@ -26,7 +40,7 @@ class Form {
 
     this._running_scripts[name] = parse(script);
 
-    // on_run_script
+    this.event_emitter.emit("script_run", name);
   }
 
   _parse() {
@@ -39,6 +53,71 @@ class Form {
     this._rules = source.rules;
     this._signals = source.signals;
     this._events = source.events;
+
+    const process_actions = actions => {
+      for (const [action_name, action_value] of Object.entries(actions)) {
+        if (action_name === "script_run") {
+          this._run_script(action_value);
+        }
+      }
+    };
+
+    for (const rule of this._rules) {
+      for (const [trigger_name, trigger_value] of Object.entries(
+        rule.triggers
+      )) {
+        const internal_rules = ["form_init", "script_run"];
+        if (internal_rules.includes(trigger_name)) {
+          this.event_emitter.on(trigger_name, value => {
+            if (value > trigger_value) process_actions(rule.actions);
+          });
+        } else {
+          this.root.rules_event_emitter.on(trigger_name, value => {
+            if (value > trigger_value) process_actions(rule.actions);
+          });
+        }
+      }
+    }
+
+    for (const signal of this._signals) {
+      for (const [trigger_name, trigger_value] of Object.entries(
+        signal.triggers
+      )) {
+        if ("min" in trigger_value && "max" in trigger_value) {
+          this.root.signals_event_emitter.on(trigger_name, value => {
+            if (value > trigger_value.min && value < trigger_value.min)
+              process_actions(signal.actions);
+          });
+        } else if ("value") {
+          this.root.signals_event_emitter.on(trigger_name, value => {
+            if (value === trigger_value.value) process_actions(signal.actions);
+          });
+        } else {
+          throw `Wrong trigger[${trigger_name}] value: ${trigger_value}` +
+            ` of signal[${signal}]`;
+        }
+      }
+    }
+
+    for (const event of this._events) {
+      for (const [trigger_name, trigger_value] of Object.entries(
+        event.triggers
+      )) {
+        if ("min" in trigger_value && "max" in trigger_value) {
+          this.root.events_event_emitter.on(trigger_name, value => {
+            if (value > trigger_value.min && value < trigger_value.min)
+              process_actions(event.actions);
+          });
+        } else if ("value") {
+          this.root.events_event_emitter.on(trigger_name, value => {
+            if (value === trigger_value.value) process_actions(event.actions);
+          });
+        } else {
+          throw `Wrong trigger[${trigger_name}] value: ${trigger_value}` +
+            ` of event[${event}]`;
+        }
+      }
+    }
   }
 }
 
--- test/scripting_system/forms_test.js
+++ test/scripting_system/forms_test.js
@@ -5,11 +5,15 @@ const Form = require("../../src/scripting_system/form");
 const {
   RETURN_CODE
 } = require("../../src/scripting_system/instruction/return_code");
+const EventEmitter = require("events");
 
 const forms_full_name = path.join(__dirname, "forms_test.json");
 
 let root = {
-  forms_source: {}
+  forms_source: {},
+  rules_event_emitter: new EventEmitter(),
+  signals_event_emitter: new EventEmitter(),
+  events_event_emitter: new EventEmitter()
 };
 
 describe("Forms test", () => {
@@ -17,9 +21,10 @@ describe("Forms test", () => {
     const forms_source_array = Util.read_from_json(forms_full_name);
     for (const source of forms_source_array)
       root.forms_source[source.name] = source;
+    console.log({ forms_source_array, forms_source: root.forms_source });
   });
   it("Parse form", () => {
-    const form = new Form(root.forms_source["Test_1"]);
+    const form = new Form(root, root.forms_source["Test_1"]);
 
     expect(form.id).to.equal("Test_1_ID");
     expect(form._name).to.equal("Test_1");
@@ -30,7 +35,7 @@ describe("Forms test", () => {
   });
 
   it("Process form", () => {
-    const form = new Form(root.forms_source["Test_1"]);
+    const form = new Form(root, root.forms_source["Test_1"]);
     form.process(root);
   });
 });
