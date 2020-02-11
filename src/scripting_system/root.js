11810429228fbcc613da72197cc2adcf04f1e1a8
Piotr Rydwański <pioryd123@gmail.com>
2020-02-11 01:38:48 +0100

    Update root.js

--- src/scripting_system/root.js
+++ src/scripting_system/root.js
@@ -1,5 +1,7 @@
 const ObjectID = require("bson-objectid");
 const ReturnValues = require("./return_values");
+const System = require("./system");
+const Form = require("./form");
 const EventEmitter = require("events");
 
 /**
@@ -10,9 +12,11 @@ const EventEmitter = require("events");
 
 class Root {
   constructor() {
+    this.system = {};
     this.forms = {};
     this.api = {};
-    this.system = {};
+    this.data = {};
+    this.ext = {};
 
     this.return_values = new ReturnValues();
     this.signals_event_emitter = new EventEmitter();
@@ -21,6 +25,34 @@ class Root {
 
   generate_unique_id() {
     return ObjectID().toHexString();
+  }
+
+  process() {
+    this.system.process();
+  }
+
+  terminate() {
+    this.system.terminate();
+  }
+
+  install_system(source) {
+    this.system = new System(this, source);
+  }
+
+  install_forms(source) {
+    for (const form of source) this.forms[form.name] = new Form(this, source);
+  }
+
+  install_api(source) {
+    this.api = source;
+  }
+
+  install_data(source) {
+    this.data = source;
+  }
+
+  install_ext(source) {
+    this.ext = { ...this.ext, ...source };
   }
 }
 
