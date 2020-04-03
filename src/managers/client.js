7ca1de50b5a1a8f442a8ae17e43fab4fcd540a22
Piotr Rydwański <pioryd123@gmail.com>
2020-04-04 00:56:16 +0200

    Add app client manager

---
+++ src/managers/client.js
@@ -0,0 +1,54 @@
+const { Client } = require("../net/client");
+
+class ClientManager {
+  constructor({ root_module, config, parse_packet, on_connected }) {
+    this.root_module = root_module;
+    this.config = config;
+    this.parse_packet = parse_packet;
+    this.on_connected = on_connected;
+
+    this.client = new Client({
+      url: config.url,
+      options: config.options
+    });
+  }
+
+  initialize() {
+    this.client.events.connected = this.on_connected;
+    this.client.add_parse_packet_dict(this._create_parse_packet_dict());
+
+    this._connect();
+  }
+
+  terminate() {
+    this._disconnect();
+  }
+
+  poll() {
+    this.client.poll();
+  }
+
+  _connect() {
+    this.client.connect();
+  }
+
+  _disconnect() {
+    this.client.disconnect();
+  }
+
+  _create_parse_packet_dict() {
+    let parse_packet_dict = {};
+    for (const [packet_id] of Object.entries(this.parse_packet)) {
+      parse_packet_dict[packet_id] = data => {
+        return this.parse_packet[packet_id](data, this.root_module.managers);
+      };
+    }
+    return parse_packet_dict;
+  }
+
+  send(packet_id, data) {
+    this.client.send(packet_id, data);
+  }
+}
+
+module.exports = ClientManager;
--- src/managers/index.js
+++ src/managers/index.js
@@ -1,7 +1,9 @@
-const AdminServer = require("./admin_server");
+const Client = require("./client");
 const Server = require("./server");
+const AdminServer = require("./admin_server");
 
 module.exports = {
+  Client,
   Server,
   AdminServer
 };
