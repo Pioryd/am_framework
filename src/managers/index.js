6cb44b85a90569915371882138c871fd6cdcddab
Piotr Rydwański <pioryd123@gmail.com>
2020-04-11 12:26:54 +0200

    Add database managers

--- src/managers/index.js
+++ src/managers/index.js
@@ -1,10 +1,14 @@
 const DatabaseStatic = require("./database_static");
+const DatabaseDynamic = require("./database_dynamic");
+const DatabaseScripts = require("./database_scripts");
 const Client = require("./client");
 const Server = require("./server");
 const AdminServer = require("./admin_server");
 
 module.exports = {
   DatabaseStatic,
+  DatabaseDynamic,
+  DatabaseScripts,
   Client,
   Server,
   AdminServer
