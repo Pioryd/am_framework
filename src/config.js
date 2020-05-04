f33abb635e0564821bd2d6df5bac814200c33c10
Piotr Rydwański <pioryd123@gmail.com>
2020-05-04 22:19:35 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,33 @@ const _ = require("lodash");
 
 const { Util } = require("./util");
 
+const logger = require("./logger").create_logger({
+  module_name: "am_framework",
+  file_name: __filename
+});
+
+const INIT_CONFIG = {
+  modules: {
+    module_example: {
+      managers_order: {
+        initialize: ["admin_server"],
+        terminate: ["admin_server"],
+        poll: ["admin_server"]
+      },
+      managers_map: {
+        admin_server: {
+          login: "admin",
+          password: "123",
+          options: {
+            port: 3101,
+            packet_timeout: 0
+          },
+          socket_io_options: {}
+        }
+      }
+    }
+  }
+};
 class Config {
   constructor({
     default_data = {},
@@ -20,7 +47,7 @@ class Config {
   }
 
   _read_data_from_files() {
-    if (this.folder_full_name != null) {
+    if (this.folder_full_name != null && fs.existsSync(this.folder_full_name)) {
       for (const file of Util.get_files(this.folder_full_name))
         this.data = _.merge(
           Util.read_from_json(path.join(this.folder_full_name, file)),
@@ -29,9 +56,15 @@ class Config {
     }
 
     this.data = _.merge(this.data, this.default_data);
-    this.data = _.merge(this.data, Util.read_from_json(this.file_full_name));
-  }
 
+    if (!fs.existsSync(this.file_full_name)) {
+      logger.info("Not found config.json. Create new one.");
+      Util.write_to_json(this.file_full_name, INIT_CONFIG);
+      this.data = _.merge(this.data, INIT_CONFIG);
+    } else {
+      this.data = _.merge(this.data, Util.read_from_json(this.file_full_name));
+    }
+  }
   load() {
     this._read_data_from_files();
 
